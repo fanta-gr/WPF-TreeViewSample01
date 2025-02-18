229cf3a4ef92c3e264e29e497668c63c35110f26
Rararaghi-Tg <et176.tech@gmail.com>
2025-02-18 23:48:29 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/Model/FileSystemItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class FileSystemItem : INotifyPropertyChanged
+    {
+        private bool _isChecked;
+        private ObservableCollection<FileSystemItem> _children;
+
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public bool IsDirectory { get; set; }
+
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set
+            {
+                if (_isChecked != value)
+                {
+                    _isChecked = value;
+                    OnPropertyChanged(nameof(IsChecked));
+
+                    // 子要素のチェック状態を更新
+                    foreach (var child in Children)
+                    {
+                        child.IsChecked = value;
+                    }
+                }
+            }
+        }
+
+        public ObservableCollection<FileSystemItem> Children
+        {
+            get
+            {
+                if (_children == null)
+                {
+                    _children = new ObservableCollection<FileSystemItem>();
+                }
+                return _children;
+            }
+            set
+            {
+                _children = value;
+                OnPropertyChanged(nameof(Children));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<FileSystemItem> _items;
+
+        public ObservableCollection<FileSystemItem> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged(nameof(Items));
+            }
+        }
+
+        public MainViewModel()
+        {
+            LoadFileSystem(@"D:\Contents\競馬");
+        }
+
+        private void LoadFileSystem(string rootPath)
+        {
+            if (Directory.Exists(rootPath))
+            {
+                Items = new ObservableCollection<FileSystemItem>();
+                var root = new DirectoryInfo(rootPath);
+                Items.Add(CreateFileSystemItem(root));
+            }
+        }
+
+        private FileSystemItem CreateFileSystemItem(DirectoryInfo directoryInfo)
+        {
+            var item = new FileSystemItem
+            {
+                Name = directoryInfo.Name,
+                FullPath = directoryInfo.FullName,
+                IsDirectory = true
+            };
+
+            // フォルダのサブフォルダを追加
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                item.Children.Add(CreateFileSystemItem(dir));
+            }
+
+            // フォルダ内のファイルを追加
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                item.Children.Add(new FileSystemItem
+                {
+                    Name = file.Name,
+                    FullPath = file.FullName,
+                    IsDirectory = false
+                });
+            }
+
+            return item;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
